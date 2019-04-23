5e688a7e7aa5179e75803eb07e902632bc62fcc0
windy <windyfancy9@126.com>
2019-04-23 13:31:42 +0800

    为添加count功能

--- test/app.js
+++ test/app.js
@@ -1,2 +1,23 @@
 var WebContext=require("../lib/webapplication.js");
-new WebContext().listen();
+var app=new WebContext();
+app.listen();
+
+
+app.onRequest("/test/count",function (ctx){
+    ctx.database.count("todo_list").then(function (count){
+        ctx.response.body="count:"+count;
+    })
+
+})
+
+app.onRequest("/test/exists",function (ctx){
+    ctx.database.exists("todo_list",{id:5}).then(function (e){
+        ctx.response.body="exists:"+e;
+    })
+})
+
+app.onRequest("/test/selectWithCount",function (ctx){
+    ctx.database.select("todo_list",{id:5},{count:true}).then(function (e){
+        ctx.render(e);
+    })
+})
